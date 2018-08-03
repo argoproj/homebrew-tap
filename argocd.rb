1e7ce319fed10fb7d0a5ed6ba7b50c26c35a0b7d
Jesse Suen <jessesuen@gmail.com>
2018-08-03 13:57:19 -0700

    Update argocd formula to v0.7.1

--- argocd.rb
+++ argocd.rb
@@ -1,9 +1,9 @@
 class Argocd < Formula
     desc "GitOps Continuous Delivery for Kubernetes"
     homepage "https://argoproj.io"
-    url "https://github.com/argoproj/argo-cd/releases/download/v0.7.0/argocd-darwin-amd64"
-    sha256 "c9e6e48565f2b2cde982ab772a8833ec793e98a74c519d183027312832f6b8c2"
-    version "0.7.0"
+    url "https://github.com/argoproj/argo-cd/releases/download/v0.7.1/argocd-darwin-amd64"
+    sha256 "707f20fcf430c7da1c7ec6e51da954e402f983838add521bdd2b256984bed9a9"
+    version "0.7.1"
 
     bottle :unneeded
 
