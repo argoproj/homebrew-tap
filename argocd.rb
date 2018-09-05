588296a9d22f8ea332f698cd2ba8e5fedbbccfc7
Jesse Suen <jessesuen@gmail.com>
2018-09-04 18:18:40 -0700

    Update argocd to v0.8.0

--- argocd.rb
+++ argocd.rb
@@ -1,9 +1,9 @@
 class Argocd < Formula
     desc "GitOps Continuous Delivery for Kubernetes"
     homepage "https://argoproj.io"
-    url "https://github.com/argoproj/argo-cd/releases/download/v0.7.2/argocd-darwin-amd64"
-    sha256 "924c171c8aad2c63654ae595bc4e8a9c5e382781c93d9efb675ac505566257f7"
-    version "0.7.2"
+    url "https://github.com/argoproj/argo-cd/releases/download/v0.8.0/argocd-darwin-amd64"
+    sha256 "bf6db6cc6fc05799686f297ec6d77e931db49055b6e1d9cfe74b55fc4ae9b2cf"
+    version "0.8.0"
 
     bottle :unneeded
 
