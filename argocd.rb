aa90758dfa5a558949a03a0347a66d238b6a930a
Alex Collins <alex_collins@intuit.com>
2019-06-14 10:20:13 -0700

    Update argocd to v1.0.2

--- argocd.rb
+++ argocd.rb
@@ -2,25 +2,14 @@
 class Argocd < Formula
     desc "GitOps Continuous Delivery for Kubernetes"
     homepage "https://argoproj.io"
-    url "https://github.com/argoproj/argo-cd/releases/download/v1.0.1/argocd-darwin-amd64"
-    sha256 "bf568447e423d730ca0325fcb718edf343ffbfa4d43ab7d7ab48868094193935"
-    version "1.0.1"
+    url "https://github.com/argoproj/argo-cd/releases/download/v1.0.2/argocd-darwin-amd64"
+    sha256 "1f820224d35e9ddc6898a32dcb4533d31d4c62a984233835e538a2f9fc116263"
+    version "1.0.2"
 
     bottle :unneeded
 
     def install
         bin.install "argocd-darwin-amd64"
         mv bin/"argocd-darwin-amd64", bin/"argocd"
-
-        # Ensure argocd is executable
-        FileUtils.chmod("+x","#{bin}/argocd")
-
-        # Install bash completion
-        output = Utils.popen_read("#{bin}/argocd completion bash")
-        (bash_completion/"argocd").write output
-
-        # Install zsh completion
-        output = Utils.popen_read("#{bin}/argocd completion zsh")
-        (zsh_completion/"_argocd").write output
     end
 end
