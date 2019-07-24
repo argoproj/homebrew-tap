5203c476ea67936ef25f161e57f92473f39cc53c
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-07-24 10:45:35 -0700

    Update argocd to v1.1.1

--- argocd.rb
+++ argocd.rb
@@ -2,9 +2,9 @@
 class Argocd < Formula
     desc "GitOps Continuous Delivery for Kubernetes"
     homepage "https://argoproj.io"
-    url "https://github.com/argoproj/argo-cd/releases/download/v1.0.2/argocd-darwin-amd64"
-    sha256 "1f820224d35e9ddc6898a32dcb4533d31d4c62a984233835e538a2f9fc116263"
-    version "1.0.2"
+    url "https://github.com/argoproj/argo-cd/releases/download/v1.1.1/argocd-darwin-amd64"
+    sha256 "2fb672aa496ca9807a5afe31367ff60cfd946228f5504dc3571ffcd5253788e4"
+    version "1.1.1"
 
     bottle :unneeded
 
