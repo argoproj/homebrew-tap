0cdc4eed495573cf75ad25a7aefc38c157761ee0
Clemens Lange <clemens.lange@cern.ch>
2019-10-23 22:48:54 +0200

    Update argo to version 2.4.1 (#6)

--- argo.rb
+++ argo.rb
@@ -1,9 +1,9 @@
 class Argo < Formula
     desc "Get stuff done with container-native workflows for Kubernetes."
     homepage "https://argoproj.io"
-    url "https://github.com/argoproj/argo/releases/download/v2.3.0/argo-darwin-amd64"
-    sha256 "f74402daa95168bbb9f6c62117a28b8260da5e2357d71681d9f5b458fb497542"
-    version "2.3.0"
+    url "https://github.com/argoproj/argo/releases/download/v2.4.1/argo-darwin-amd64"
+    sha256 "fceff61499b1c0d0609ba9b7b09506974a544ce1b9c6efaecc1372e425a9cb8e"
+    version "2.4.1"
 
     bottle :unneeded
 
@@ -13,11 +13,11 @@ class Argo < Formula
 
         # Ensure argo is executable
         FileUtils.chmod("+x","#{bin}/argo")
-        
+
         # Install bash completion
         output = Utils.popen_read("#{bin}/argo completion bash")
         (bash_completion/"argo").write output
-        
+
         # Install zsh completion
         output = Utils.popen_read("#{bin}/argo completion zsh")
         (zsh_completion/"_argo").write output
