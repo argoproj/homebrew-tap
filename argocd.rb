c77c475aebf8e46bfb383818b3ff223e06c8a54a
stgarf <stgarf@users.noreply.github.com>
2019-11-05 14:11:16 -0500

    Push Latest ArgoCD Binary via Brew (#7)

--- argocd.rb
+++ argocd.rb
@@ -3,20 +3,20 @@ class Argocd < Formula
     desc "GitOps Continuous Delivery for Kubernetes"
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-cd/releases/download"
-    version "1.2.4"
+    version "1.2.5"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "1d2967abe2ea9ef3bf3d478e1c1d989f99648720039976fb9d9393b5953ad88b"
+      sha256 "586990c261dbedbe8fd9ca6a7d526cbebc8f0f9a061bd152194b9838a2394aad"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "6d7192a6b00a36963737c68157cc7c98c4fe6cca9fc4f8df5a5d14249d3d846e"
+      sha256 "e02dd551d53759d643ea7ac058288bd32d872860f2de66737e0fbde128f49504"
     end
 
     @@bin_name = "argocd-" + kernel + "-amd64"
-    url baseurl + "/v1.2.4/" + @@bin_name
+    url baseurl + "/v1.2.5/" + @@bin_name
 
     def install
       bin.install @@bin_name
