8e5571d0f5fbd89de2b257ca71410ffc5b1ca399
tmk-hsn <tomoki213@gmail.com>
2020-03-03 07:44:12 +0900

    update to v2.6.0

--- argo.rb
+++ argo.rb
@@ -3,20 +3,20 @@ class Argo < Formula
     desc "Get stuff done with container-native workflows for Kubernetes."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo/releases/download"
-    version "v2.5.2"
+    version "v2.6.0"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "59115563b3de7582e9684902532e8b9021ee270723884837022ef43a654c0573"
+      sha256 "4d1f358909c765c07200668d84e909b7f1446ed766b872aabb850832f0562c3a"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "5a912a333e686403cff3c6ed78ed7db5475b69a8680c578f07dc5072eadd1cd0"
+      sha256 "184d60c200a341cd61513f4d49efc33608234ce51d0fc659e910020ecf0b9560"
     end
 
     @@bin_name = "argo-" + kernel + "-amd64"
-    url baseurl + "/v2.5.2/" + @@bin_name
+    url baseurl + "/v2.6.0/" + @@bin_name
 
     def install
       bin.install @@bin_name
