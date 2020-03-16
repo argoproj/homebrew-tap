a1831511dad1b6193d7d27fc4e0131f6b7fcfe6d
Alex Collins <alex_collins@intuit.com>
2020-03-16 11:28:41 -0700

    Upgraded argo to v2.6.3

--- argo.rb
+++ argo.rb
@@ -9,7 +9,7 @@ class Argo < Formula
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "05113734ac2efc1f89878483b3b2bc8f1118a551182c2cab6e3bf5b538613a64"
     elsif OS.linux?
       kernel = "linux"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
