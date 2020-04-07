1c7df190fa4c0f739e78d92049ffd5829e32b3d5
Alex Collins <alex_collins@intuit.com>
2020-04-07 10:18:21 -0700

    upgrade argo to v2.7.1

--- argo.rb
+++ argo.rb
@@ -3,20 +3,20 @@ class Argo < Formula
     desc "Get stuff done with container-native workflows for Kubernetes."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo/releases/download"
-    version "v2.7.0"
+    version "v2.7.1"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "1676fed8d1224732b5ddc29821f07512810a67c648bc50d5d4b2761f1bf5a356"
+      sha256 "b987aab3253fe39c7b775f95e750ebc06b3564ecb829cf2fef06562d7a0fd31e"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "e28bff15fe6e101b02167920bc2898206493e7e9e686d4b762a98cfe6b085b09"
+      sha256 "d871d4005105a47e81ab446047c05f4aa97738a62ecd972d56ee5e61f24de4ea"
     end
 
     @@bin_name = "argo-" + kernel + "-amd64"
-    url baseurl + "/v2.7.0/" + @@bin_name
+    url baseurl + "/v2.7.1/" + @@bin_name
 
     def install
       bin.install @@bin_name
