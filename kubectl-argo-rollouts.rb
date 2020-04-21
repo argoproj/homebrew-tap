61afddb924cd79c315c0f16e595333c94502cc6c
Danny Thomson <dthomson25@gmail.com>
2020-04-20 17:11:01 -0700

    Update kubectl-argo-rollouts to v0.8.1

--- kubectl-argo-rollouts.rb
+++ kubectl-argo-rollouts.rb
@@ -3,20 +3,20 @@ class KubectlArgoRollouts < Formula
     desc "Kubectl Argo Rollouts Plugin."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v0.8.0"
+    version "v0.8.1"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "f390fd3fb58012e3ab60a94255b4c5e96a748fac5e24608b9d9f1b08e4c0524b"
+      sha256 "6f8ff5d9b1a43ba18058bb636ba5a78f21ac3ebccf5682a125840223dfb89726"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "70d5cb925363f5dd51a3bdf144ed352e4150e6b2eb94d8c76db86c1e6ab98ba9"
+      sha256 "6900811f502eeccd7bc407cd06a0f92164e65c3072fe3d17af450271899e8060"
     end
 
     @@bin_name = "kubectl-argo-rollouts-" + kernel + "-amd64"
-    url baseurl + "/v0.8.0/" + @@bin_name
+    url baseurl + "/v0.8.1/" + @@bin_name
 
     def install
       bin.install @@bin_name
