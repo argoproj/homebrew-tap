96945c8f20d476d24c5dab6d533ec2e5e00fedc1
Alexander Matyushentsev <amatyushentsev@gmail.com>
2020-05-01 22:02:36 -0700

    Update argocd to v1.5.3

--- argocd.rb
+++ argocd.rb
@@ -3,20 +3,20 @@ class Argocd < Formula
     desc "GitOps Continuous Delivery for Kubernetes"
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-cd/releases/download"
-    version "v1.5.2"
+    version "v1.5.3"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "6794682fbdcd7cc34145fceab388149d66edc5308fd5dec516e9e01fa30d09e4"
+      sha256 "d5e16b9edb8222e89ee5405ccfd7f55b2495ac58e857cbcfa5b9566d6204d23d"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "b50607f3f81347cc25ba7812fa1cbeaaa5ff0e29ce71da8f4ed474200be867b9"
+      sha256 "bf038fba5984c3315a4d0371188014e31b2d41552bd830d24214a6a6d95db1fc"
     end
 
     @@bin_name = "argocd-" + kernel + "-amd64"
-    url baseurl + "/v1.5.2/" + @@bin_name
+    url baseurl + "/v1.5.3/" + @@bin_name
 
     def install
       bin.install @@bin_name
