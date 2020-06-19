a669a529d3449806eeaec1bd12af96f6470b10f4
Simon Behar <simbeh7@gmail.com>
2020-06-19 09:13:35 -0700

    Update Argo to v2.8.2

--- argo.rb
+++ argo.rb
@@ -3,20 +3,20 @@ class Argo < Formula
     desc "Get stuff done with container-native workflows for Kubernetes."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo/releases/download"
-    version "v2.8.1"
+    version "v2.8.2"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "15dd9013654ea2a786d7eafc6f05e51e994ec2e7e595d84c27113a5f69eee918"
+      sha256 "6385a7f4b31a03564add64caf8f588729ceed88513838bbe8039acde5e6bcc89"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "b6c3a4a16d329de09259a1b2c60fe1917f67e8915e58ce79a9e9d7120d505899"
+      sha256 "d5673786e06639d700493ad369b4cd2a8a6cb2eef5f0c3487cc2802b0fe0991b"
     end
 
     @@bin_name = "argo-" + kernel + "-amd64"
-    url baseurl + "/v2.8.1/" + @@bin_name
+    url baseurl + "/v2.8.2/" + @@bin_name
 
     def install
       bin.install @@bin_name
