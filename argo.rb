e05232708073829d97b4d419289f46e2bbe6f46b
Alex Collins <alex_collins@intuit.com>
2020-07-01 18:43:41 -0700

    master

--- argo.rb
+++ argo.rb
@@ -3,20 +3,20 @@ class Argo < Formula
     desc "Get stuff done with container-native workflows for Kubernetes."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo/releases/download"
-    version "v2.8.2"
+    version "v2.9.0"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "6385a7f4b31a03564add64caf8f588729ceed88513838bbe8039acde5e6bcc89"
+      sha256 "39d7c1ddb05ce3386d4e5a0299c707cccbc575dbd12ea63b9a6c53eb1662ac88"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "d5673786e06639d700493ad369b4cd2a8a6cb2eef5f0c3487cc2802b0fe0991b"
+      sha256 "c05814cfc4b014e5e6986873c077e3a7205f129961bf8ef3763b1c0c5abed72d"
     end
 
     @@bin_name = "argo-" + kernel + "-amd64"
-    url baseurl + "/v2.8.2/" + @@bin_name
+    url baseurl + "/v2.9.0/" + @@bin_name
 
     def install
       bin.install @@bin_name
