ed5bff09c0bbf1f05ef77a76c2a38d250665383b
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2020-08-26 10:36:48 -0700

    Update argocd to v1.7.0

--- argocd.rb
+++ argocd.rb
@@ -3,20 +3,20 @@ class Argocd < Formula
     desc "GitOps Continuous Delivery for Kubernetes"
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-cd/releases/download"
-    version "v1.6.0"
+    version "v1.7.0"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "ec9e9375d95deb199febb77cf06209fbe1d311a87513add2b45e27692e66a363"
+      sha256 "4c3ec8ae59aa6dfa9c62a62de9ade8a14e75af56a4e8b939a3e774c2ac0f5e4b"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "9d305b644dd091a48bf13f5216c94c448b6bec90ba0b61fb5e8238e5aab86379"
+      sha256 "a28579d03ad8d87fc726cf018178117f4650b643ddc4b1d2397a0f7d59834211"
     end
 
     @@bin_name = "argocd-" + kernel + "-amd64"
-    url baseurl + "/v1.6.0/" + @@bin_name
+    url baseurl + "/v1.7.0/" + @@bin_name
 
     def install
       bin.install @@bin_name
