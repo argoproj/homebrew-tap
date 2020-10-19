068a1dc70e1b731faec1c63776f702e05ec720b2
Jesse Suen <jesse_suen@intuit.com>
2020-10-18 17:49:43 -0700

    Update kubectl-argo-rollouts plugin to v0.9.2. Migrate argocd formula to homebrew-core

--- kubectl-argo-rollouts.rb
+++ kubectl-argo-rollouts.rb
@@ -3,20 +3,20 @@ class KubectlArgoRollouts < Formula
     desc "Kubectl Argo Rollouts Plugin."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v0.9.1"
+    version "v0.9.2"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "9d6f3cade577e440a79f9852788aa2c188c5411f883953f379035d33926008ed"
+      sha256 "87ddb93a5c4c3d24d11672945916b0a35a0911ca48e64792625c258807f75eef"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "b51e22134c624cb075bfe123dcdd5563bbad18e4e8d69160af10eee88ce9abf7"
+      sha256 "522b83bb48ea0cda56c06f003f4a6f2d2829568c0c75dad4378ec0e81e38943a"
     end
 
     @@bin_name = "kubectl-argo-rollouts-" + kernel + "-amd64"
-    url baseurl + "/v0.9.1/" + @@bin_name
+    url baseurl + "/v0.9.2/" + @@bin_name
 
     def install
       bin.install @@bin_name
