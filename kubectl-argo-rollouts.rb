505cf87f033f9aa0a726bbf967205e27f3d366db
Jesse Suen <jesse_suen@intuit.com>
2020-11-19 09:33:08 -0800

    chore: update kubectl-argo-rollouts plugin to v0.10.0
    Signed-off-by: Jesse Suen <jesse_suen@intuit.com>

--- kubectl-argo-rollouts.rb
+++ kubectl-argo-rollouts.rb
@@ -3,20 +3,20 @@ class KubectlArgoRollouts < Formula
     desc "Kubectl Argo Rollouts Plugin."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v0.9.2"
+    version "v0.10.0"
 
     bottle :unneeded
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "87ddb93a5c4c3d24d11672945916b0a35a0911ca48e64792625c258807f75eef"
+      sha256 "c93dc62836d7e4bbe98a466dd5889fb4a5cf41c010c2dcd29eb23913430e1377"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "522b83bb48ea0cda56c06f003f4a6f2d2829568c0c75dad4378ec0e81e38943a"
+      sha256 "5c2e110bbce23b339802a7ccd79d286556bf08b01ec67d02ba4df891171ba4bd"
     end
 
     @@bin_name = "kubectl-argo-rollouts-" + kernel + "-amd64"
-    url baseurl + "/v0.9.2/" + @@bin_name
+    url baseurl + "/v0.10.0/" + @@bin_name
 
     def install
       bin.install @@bin_name
