841511ef1e0743f91ae8132e23e7e8796e8d1bf9
zachaller <zachaller@hotmail.com>
2022-09-06 12:41:12 -0500

    Release 1.3-rc2
    Signed-off-by: zachaller <zachaller@hotmail.com>

--- kubectl-argo-rollouts.rb
+++ kubectl-argo-rollouts.rb
@@ -3,18 +3,18 @@ class KubectlArgoRollouts < Formula
     desc "Kubectl Argo Rollouts Plugin."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v1.2.2"
+    version "v1.3.0-rc2"
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "a30b1b33829e2a4ee6ca0c4dd4d99f0dc654449d9714926fcb2e7b504aff37e0"
+      sha256 "39c0e3d6c8e4770f5f26f89dc1b915c33deb26012de7d1164ff08d8590fe83b4"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "43ce6ab7105c3161690cd810c7a675994d0fb96527ee38fb76722393e61b29e1"
+      sha256 "832036e4f239165c54f5a821c9de720621ce68c618605669c07b9a5a8b96be71"
     end
 
     @@bin_name = "kubectl-argo-rollouts-" + kernel + "-amd64"
-    url baseurl + "/v1.2.2/" + @@bin_name
+    url baseurl + "/v1.3.0-rc2/" + @@bin_name
 
     def install
       bin.install @@bin_name
