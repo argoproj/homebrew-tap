fad613ec168bb9e990a789a1ed08ed4aaeeb75f0
Zach Aller <zach_aller@intuit.com>
2024-04-08 15:03:02 -0500

    don't upgrade non versioned installes
    Signed-off-by: Zach Aller <zach_aller@intuit.com>

--- kubectl-argo-rollouts.rb
+++ kubectl-argo-rollouts.rb
@@ -3,18 +3,18 @@ class KubectlArgoRollouts < Formula
     desc "Kubectl Argo Rollouts Plugin."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v1.7.0-rc1"
+    version "v1.6.0"
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "637d9fcda4fc8380b908278ed691b7c52b767bb6a966b0869fbc4c38e722be36"
+      sha256 "915bc4951741e7ac0760448a48590f455ff5cc4a2ae0fd5834ded74080bc4402"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "0a329c8540b79b01dc58d2533e8a6f2130107ecb97cd04c0eff8527689c60e58"
+      sha256 "690cff6930282d5898ee3b13e2f446b1cf7d3a1eafbb840be9185c599ab22d73"
     end
 
     @@bin_name = "kubectl-argo-rollouts-" + kernel + "-amd64"
-    url baseurl + "/v1.7.0-rc1/" + @@bin_name
+    url baseurl + "/v1.6.0/" + @@bin_name
 
     def install
       bin.install @@bin_name
