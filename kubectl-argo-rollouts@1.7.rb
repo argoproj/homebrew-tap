e01fd0a3a814e262a6cd2bac166bc4b3a38df347
Zach Aller <zach_aller@intuit.com>
2024-08-05 11:53:54 -0500

    update tap to rollouts 1.7.1
    Signed-off-by: Zach Aller <zach_aller@intuit.com>

--- kubectl-argo-rollouts@1.7.rb
+++ kubectl-argo-rollouts@1.7.rb
@@ -3,18 +3,18 @@ class KubectlArgoRolloutsAT17 < Formula
     desc "Kubectl Argo Rollouts Plugin."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v1.7.0-rc1"
+    version "v1.7.1"
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "637d9fcda4fc8380b908278ed691b7c52b767bb6a966b0869fbc4c38e722be36"
+      sha256 "173f56252e6d08fe564638a0e28360994430e4ca444713bd5ccfe6392d4a02fa"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "0a329c8540b79b01dc58d2533e8a6f2130107ecb97cd04c0eff8527689c60e58"
+      sha256 "b42859a4ead2b02dc1a53a101490f60adc9915b602e033ddc49e78e74a20895b"
     end
 
     @@bin_name = "kubectl-argo-rollouts-" + kernel + "-amd64"
-    url baseurl + "/v1.7.0-rc1/" + @@bin_name
+    url baseurl + "/v1.7.1/" + @@bin_name
 
     def install
       bin.install @@bin_name
