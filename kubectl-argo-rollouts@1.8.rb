87fbd7b62474016090a3ad5556c66db8c1009244
Oleg Andreyev <oleg.andreyev@lampa.lv>
2025-08-17 17:40:49 +0300

    update to 1.8.3
    Signed-off-by: Oleg Andreyev <oleg.andreyev@lampa.lv>

--- kubectl-argo-rollouts@1.8.rb
+++ kubectl-argo-rollouts@1.8.rb
@@ -3,18 +3,18 @@ class KubectlArgoRolloutsAT18 < Formula
     desc "Kubectl Argo Rollouts Plugin."
     homepage "https://argoproj.io"
     baseurl = "https://github.com/argoproj/argo-rollouts/releases/download"
-    version "v1.8.1"
+    version "v1.8.3"
 
     if OS.mac?
       kernel = "darwin"
-      sha256 "e1e2dec9b3c7c334c937b7450a4d3e8321aab6a217031c504fb78338c25a3618"
+      sha256 "78562a0f9ce18d2253216c0114a5fd90a597655dacf0bad2a6942c2c9e5a5109"
     elsif OS.linux?
       kernel = "linux"
-      sha256 "d2681d3c22d0c143481924d1332d07ced7f0828b89a85e28b9be598ae25950b7"
+      sha256 "6f60f3608ffc1e6b59c05ffc979d80ad1acd886d7fcbffb1e8e0c50291674373"
     end
 
     @@bin_name = "kubectl-argo-rollouts-" + kernel + "-amd64"
-    url baseurl + "/v1.8.1/" + @@bin_name
+    url baseurl + "/v1.8.3/" + @@bin_name
 
     def install
       bin.install @@bin_name
